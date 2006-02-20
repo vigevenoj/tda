278ed2a6397a520aa31186fb115a199f9faafc40
irockel <irockel@246db37b-da0d-4fa0-e44f-95c77f9f54d1>
2006-02-20 09:47:44 +0000

    license and basic javadoc

--- tda/src/java/com/pironet/tda/JDK14Parser.java
+++ tda/src/java/com/pironet/tda/JDK14Parser.java
@@ -1,11 +1,23 @@
 /*
  * JDK14Parser.java
  *
+ * This file is part of TDA - Thread Dump Analysis Tool.
  *
- * Created on 9. Februar 2006, 13:31
+ * Foobar is free software; you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with Foobar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id: JDK14Parser.java,v 1.2 2006-02-20 09:47:43 irockel Exp $
  */
 
 package com.pironet.tda;
@@ -28,6 +40,7 @@ import javax.swing.tree.TreePath;
 /**
  * Parses JDK14 Thread Dumps.
  * Needs to be closed after use (so inner stream is closed).
+ *
  * @author irockel
  */
 public class JDK14Parser implements DumpParser {
--- tda/src/java/com/pironet/tda/TDA.java
+++ tda/src/java/com/pironet/tda/TDA.java
@@ -1,5 +1,23 @@
 /**
- * Thread Analysis Tool, parses Thread Dump input and displays it as tree
+ * Thread Dump Analysis Tool, parses Thread Dump input and displays it as tree
+ *
+ * This file is part of TDA - Thread Dump Analysis Tool.
+ *
+ * Foobar is free software; you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with Foobar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id: TDA.java,v 1.4 2006-02-20 09:47:44 irockel Exp $
  */
 package com.pironet.tda;
 
@@ -40,6 +58,11 @@ import javax.swing.JPopupMenu;
 import javax.swing.KeyStroke;
 import javax.swing.tree.TreePath;
 
+/**
+ * main class of the Thread Dump Analyzer. Start using static main method.
+ *
+ * @author irockel
+ */
 public class TDA extends JPanel implements TreeSelectionListener, ActionListener {
     private JEditorPane htmlPane;
     private JTree tree;
