b59b3f5affd001dcf28c4f8168002a86c1076ea7
irockel <irockel@246db37b-da0d-4fa0-e44f-95c77f9f54d1>
2006-04-23 08:21:00 +0000

    added infos

--- tda/bin/tda.bat
+++ tda/bin/tda.bat
@@ -1,2 +1,9 @@
+@echo off
+rem ***********
+rem Basic start script for TDA for Windows
+rem ***********
+rem If you have big log file you might need to adjust Xmx setting
+
 cd ..
 java -Xmx256m -jar tda.jar
+cd bin
