da92f14de9586bcea37447e6318d30cb1d41a794
irockel <irockel@246db37b-da0d-4fa0-e44f-95c77f9f54d1>
2007-05-02 16:00:52 +0000

    fixed copyright notice

--- tda/src/java/com/pironet/tda/utils/TreeRenderer.java
+++ tda/src/java/com/pironet/tda/utils/TreeRenderer.java
@@ -1,10 +1,23 @@
 /*
  * TreeRenderer.java
+  *
+ * This file is part of TDA - Thread Dump Analysis Tool.
  *
- * Created on 30. April 2007, 10:29
+ * TDA is free software; you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * TDA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with TDA; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id: TreeRenderer.java,v 1.4 2007-05-02 16:00:52 irockel Exp $
  */
 
 package com.pironet.tda.utils;
