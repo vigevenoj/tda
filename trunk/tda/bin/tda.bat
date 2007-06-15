a9153770a78e866da6035bd25809ed70ec5a4681
irockel <irockel@246db37b-da0d-4fa0-e44f-95c77f9f54d1>
2007-06-15 10:45:04 +0000

    place independent start

--- trunk/tda/bin/tda.bat
+++ trunk/tda/bin/tda.bat
@@ -4,6 +4,4 @@ rem Basic start script for TDA for Windows
 rem ***********
 rem If you have big log file you might need to adjust Xmx setting
 
-cd ..
-java -Xmx256m -jar tda.jar
-cd bin
+java -Xmx256m -jar "%~dp0/../tda.jar"
