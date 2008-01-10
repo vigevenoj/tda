c35c2389ecae721583c19ae9b2698af081ad30e5
irockel <irockel@246db37b-da0d-4fa0-e44f-95c77f9f54d1>
2008-01-10 20:36:11 +0000

    fixed copyright notice

--- trunk/tda/src/java/com/pironet/tda/utils/ColoredTable.java
+++ trunk/tda/src/java/com/pironet/tda/utils/ColoredTable.java
@@ -1,8 +1,24 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * ColoredTable.java
+ *
+ * This file is part of TDA - Thread Dump Analysis Tool.
+ *
+ * TDA is free software; you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * TDA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with TDA; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id: ColoredTable.java,v 1.2 2008-01-10 20:36:11 irockel Exp $
  */
-
 package com.pironet.tda.utils;
 
 import java.awt.Color;
