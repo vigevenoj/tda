2b7d49c5b0758960bf85122b124380a25ff2bef5
irockel <irockel@246db37b-da0d-4fa0-e44f-95c77f9f54d1>
2010-01-18 17:42:45 +0000

    adjusted app ver info to 2.2

--- tda/src/java/com/pironet/tda/utils/AppInfo.java
+++ tda/src/java/com/pironet/tda/utils/AppInfo.java
@@ -17,7 +17,7 @@
  * along with TDA; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * $Id: AppInfo.java,v 1.10 2008-10-31 08:19:48 irockel Exp $
+ * $Id: AppInfo.java,v 1.11 2010-01-18 17:42:45 irockel Exp $
  */
 package com.pironet.tda.utils;
 
@@ -28,9 +28,9 @@ package com.pironet.tda.utils;
 public class AppInfo {
     private static final String APP_SHORT_NAME = "TDA";
     private static final String APP_FULL_NAME = "Thread Dump Analyzer";
-    private static final String VERSION = "2.1";
+    private static final String VERSION = "2.2";
     
-    private static final String COPYRIGHT = "2006-2008";
+    private static final String COPYRIGHT = "2006-2010";
     
     /**
      * get info text for status bar if no real info is displayed.
